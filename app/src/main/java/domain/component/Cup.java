6594cf267f75b9800b9bd480e2d333566fe9e584
Chori <105255517+ChoriDev@users.noreply.github.com>
2024-11-01 15:36:24 +0900

    더블 횟수를 세는 책임을 Cup 클래스도 이전

--- app/src/main/java/domain/component/Cup.java
+++ app/src/main/java/domain/component/Cup.java
@@ -3,14 +3,18 @@ package domain.component;
 public class Cup {
     private static Cup instance;
 
+    private final int MAX_COUNT_OF_DOUBLE = 3;
+
     private Die firstDie;
     private Die secondDie;
     private int total;
+    private int countOfDouble;
 
     private Cup() {
         this.firstDie = new Die();
         this.secondDie = new Die();
         this.total = 0;
+        this.countOfDouble = 0;
     }
 
     public static Cup getInstance() {
@@ -25,6 +29,11 @@ public class Cup {
         firstDie.roll();
         secondDie.roll();
         total = firstDie.getFaceValue() + secondDie.getFaceValue();
+        if (isDouble()) {
+            addCountOfDouble();
+        } else {
+            resetCountOfDouble();
+        }
     }
 
     private void resetTotal() {
@@ -37,5 +46,13 @@ public class Cup {
 
     public boolean isDouble() {
         return firstDie.getFaceValue() == secondDie.getFaceValue();
+    }
+
+    private void addCountOfDouble() {
+        countOfDouble++;
+    }
+
+    public void isThirdDouble() {
+        return countOfDouble == MAX_COUNT_OF_DOUBLE;
     }
 }
